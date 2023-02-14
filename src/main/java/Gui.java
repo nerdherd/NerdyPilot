a7ebee5443f2ca51e8824d982dc7cff6ebced089
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2023-02-14 15:31:34 -0800

    Add imgui node example

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -1,8 +1,12 @@
 import imgui.ImGui;
 import imgui.app.Application;
 import imgui.app.Configuration;
+import imgui.type.ImBoolean;
 
 public class Gui extends Application {
+    Graph aGraph = new Graph();
+    ImBoolean imTrue = new ImBoolean(true);
+
     @Override
     protected void configure(Configuration config) {
         config.setTitle("NerdyPilot");
@@ -11,6 +15,7 @@ public class Gui extends Application {
     @Override
     public void process() {
         ImGui.text("Hello, World!");
+        FilterGraph.show(imTrue, aGraph);
     }
 
     public static void main(String[] args) {
